2c4be866f7cf90d6d23bcae8372a576febd60a77
zeroet <y36609672@gmail.com>
2022-09-24 00:05:30 +0200

    j00 fini

--- js/j00/ex04/exid.js
+++ js/j00/ex04/exid.js
@@ -1,3 +1,19 @@
+
 const exid = {
-    count: 
-}
+    count : 0,
+    up() { this.count++ },
+    down() { this.count-- },
+    showCount() { console.log(this.count) }
+};
+
+exid.up();
+exid.down();
+exid.up();
+exid.up();
+exid.down();
+exid.up();
+exid.down();
+exid.up();
+exid.up();
+exid.down();
+exid.showCount();
