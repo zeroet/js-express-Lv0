ac4a4892500380f832b805c26e0b2a6348eb028d
zeroet <y36609672@gmail.com>
2022-10-06 22:56:39 +0200

    config key

---
+++ node_js/boiler-plate/config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://seyun:seyun123@boilerplate.fbopivw.mongodb.net/?retryWrites=true&w=majority'
+}
--- node_js/boiler-plate/index.js
+++ node_js/boiler-plate/index.js
@@ -2,14 +2,39 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const config = require('./config/key');
+
+// Model - Schema User
+const { User } = require("./models/User");
+
+// bodyParser
+const bodyParser = require('body-parser');
+// application/x-www.form-urlenconded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://seyun:seyun123@boilerplate.fbopivw.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
 }).then(()=> console.log('MongoDb Connected...'))
   .catch(err => console.log(err))
 
-  
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! hello hello')
+})
+
+app.post('/register', (req, res) => {
+  // sign up date - > put db
+
+    const user = new User(req.body)
+    // mongo db methode
+    user.save((err, doc) => {
+      if (err) return res.json({ success: false, err})
+      return res.status(200).json({
+        success: true
+      })
+    })
 })
 
 app.listen(port, () => {
