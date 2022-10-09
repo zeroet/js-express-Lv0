957ea66480d6ecd5f5d5b3f00c88b366444d1816
zeroet <y36609672@gmail.com>
2022-10-09 23:15:11 +0200

    back

--- node_js/boiler-plate/index.js
+++ node_js/boiler-plate/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-const config = require('./config/key');
+const cookiePaser = require('cookie-parser');
 
+const config = require('./config/key');
+const { auth } = require('./middlewawre/auth');
 // Model - Schema User
 const { User } = require("./models/User");
 
@@ -13,6 +15,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 // application/json
 app.use(bodyParser.json());
+app.use(cookiePaser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -24,12 +27,12 @@ app.get('/', (req, res) => {
   res.send('Hello World! hello hello')
 })
 
-app.post('/register', (req, res) => {
+app.post('api/user/register', (req, res) => {
   // sign up date - > put db
 
     const user = new User(req.body)
     // mongo db methode
-    user.save((err, doc) => {
+    user.save((err, userInfo) => {
       if (err) return res.json({ success: false, err})
       return res.status(200).json({
         success: true
@@ -37,9 +40,59 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
-  
+app.post('/api/user/login', (req, res) => {
+
+  // console.log("hiihihihihihihihi");
+  User.findOne({ email: req.body.email }, (err, userInfo) =>{
+    if (!userInfo)
+    {
+      return res.json({
+        loginSuccess: false,
+        message: "no matching user email"
+      })
+    }
+
+
+    userInfo.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+      return res.json({loginSuccess: false, message: "wrong password"})
+
+      userInfo.generateToken((err, user)=> {
+        if (err) return res.status(400).send(err);
+
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess: true, userId: user._id})
+
+      })
+    })
+
+  })
 } )
+
+app.get('/api/user/auth', auth, (req, res) => {
+
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/user/logout', auth, (req, res) => {
+  User.findOneAndUpdate({_id: req.user._id}, { token: ""}, (err, user) => {
+    if (err) return res.json({ success: false, err});
+    return res.status(200).send({
+      success: true
+    })
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- node_js/boiler-plate/models/User.js
+++ node_js/boiler-plate/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
     name: {
@@ -58,6 +58,37 @@ userSchema.pre('save', function(next)
         next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+            cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    
+    var token =  jwt.sign(user._id.toHexString(), 'secretToken')
+    
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        user.findOne({"_id": decoded, "token": token}, function (err, user){
+            if (err) return cb(err);
+            cb(null, user);
+        })
+
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
